00bc001c399013db7bda76ebf39b3c85408e6f03
lokendra2812 <erlokendra.agrawal@gmail.com>
2026-08-27 23:45:18 +0530

    added one simple program folder

---
+++ simpleCppProgram/main.cpp
@@ -0,0 +1,14 @@
+//header files included
+#include<iostream>
+
+//default namespace included
+using namespace std;
+
+//main function started from here
+int main(int argc, char *argv[]) //while running from commanline these arguments can pass
+{
+	cout<<"total number of arguments passed - "<<argc<<endl;
+	cout<<"first argument-- "<<argv[1]<<endl;
+	cout<<"second argument-- "<<argv[2]<<endl;
+}
+//end of the program
